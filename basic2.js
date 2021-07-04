21f1bd02927aada6fdbd9137174a8bc507a69bc9
wonhyuna <hyunawon417@gmail.com>
2021-07-05 03:14:48 +0900

    javascript

---
+++ basic2.js
@@ -0,0 +1,14 @@
+//추상화 -> 반복적인 것을!
+//목적을 명확히 불필요한 것들을 숨기고 핵심만 드러내기
+//!!!!변수 사용!!!!
+
+//변수 선언
+let espressoPrice = 3000; //할당 연산자
+let lattePrice = 4300;
+let mochaPrice = 4800;
+
+console.log(espressoPrice * 2);
+console.log(lattePrice * 4 + mochaPrice * 2);
+console.log(lattePrice + mochaPrice);
+console.log(espressoPrice + lattePrice * 4 + mochaPrice * 2);
+console.log(lattePrice * 3);
---
+++ basic2_practice.js
@@ -0,0 +1,15 @@
+let espresso = 10
+let milk = 170
+let chocolateSyrup = 50
+let whippedCream = 60
+
+// 메뉴별 칼로리 테스트
+
+// 에스프레소 칼로리
+console.log(espresso); 
+// 라떼 칼로리
+console.log(espresso + milk); 
+// 모카 칼로리
+console.log(espresso + chocolateSyrup + milk);
+// 모카(휘핑 추가) 칼로리
+console.log(espresso + chocolateSyrup + milk + whippedCream); 
